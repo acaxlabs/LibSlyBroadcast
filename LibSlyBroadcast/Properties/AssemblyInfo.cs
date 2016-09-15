a4877382ca62b09d421836f123955567c73f6988
Taryn Hill <Phrohdoh@gmail.com>
2016-09-15 16:47:03 -0500

    Change AssemblyCopyright from ACAXLabs to ACAExpress

--- LibSlyBroadcast/Properties/AssemblyInfo.cs
+++ LibSlyBroadcast/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("LibSlyBroadcast")]
 [assembly: AssemblyProduct("LibSlyBroadcast")]
-[assembly: AssemblyCopyright("Copyright © ACAXLabs 2016")]
+[assembly: AssemblyCopyright("Copyright © ACAExpress.com, Inc 2016")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
