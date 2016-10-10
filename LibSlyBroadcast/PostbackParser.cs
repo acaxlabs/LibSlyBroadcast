e2e48cd098b16aad3fb00907e8fdbcaee39c779f
Taryn Hill <taryn.hill@acaexpress.com>
2016-10-10 17:56:18 -0500

    Added Postback type:
      * Postback.FromEncodedString(string)
      * Postback.FromItems(string[])

---
+++ LibSlyBroadcast/PostbackParser.cs
@@ -0,0 +1,63 @@
+﻿using LibSlyBroadcast.Extensions;
+using System;
+using System.IO;
+using System.Web;
+
+namespace LibSlyBroadcast
+{
+    public class Postback
+    {
+        public string CampaignSessionId { get; set; }
+        public string DestinationPhoneNumber { get; set; }
+        public bool Successful { get; set; }
+
+        public string ReasonForFailure { get; set; }
+        public DateTime DeliveryTime { get; set; }
+
+        public string Carrier { get; set; }
+
+        public string[] ToStrings() => new[] {
+            $"{nameof(CampaignSessionId)}={CampaignSessionId}",
+            $"{nameof(DestinationPhoneNumber)}={DestinationPhoneNumber}",
+            $"{nameof(Successful)}={Successful}",
+            $"{nameof(ReasonForFailure)}={ReasonForFailure}",
+            $"{nameof(DeliveryTime)}={DeliveryTime.ToFormattedString()} America/New_York",
+            $"{nameof(Carrier)}={Carrier}"
+        };
+
+        /// <summary>Parse the postback into a useable object</summary>
+        public static Postback FromEncodedString(string encodedString)
+        {
+            var decodedString = HttpUtility.UrlDecode(encodedString);
+            var items = decodedString.Split('|');
+            return FromItems(items);
+        }
+
+        const int requiredItemCount = 6;
+
+        /// <summary>Parse the postback into a useable object</summary>
+        public static Postback FromItems(string[] items)
+        {
+            if (items.Length != requiredItemCount)
+                throw new InvalidDataException($"Length of '{nameof(items)}' must be {requiredItemCount} but is {items.Length}");
+
+            var campaignSessionId = items[0].Replace("var=", "");
+            var destinationPhoneNumber = items[1];
+            var status = items[2];
+            var reasonForFailure = items[3];
+            var deliveryTime = items[4];
+            var carrier = items[5];
+
+            DateTime dt;
+            return new Postback
+            {
+                CampaignSessionId = campaignSessionId,
+                DestinationPhoneNumber = destinationPhoneNumber,
+                Successful = string.Equals(status, "OK", StringComparison.OrdinalIgnoreCase),
+                ReasonForFailure = reasonForFailure,
+                DeliveryTime = DateTime.TryParse(deliveryTime, out dt) ? dt : default(DateTime),
+                Carrier = carrier,
+            };
+        }
+    }
+}
--- LibSlyBroadcast/SlyBroadcast.cs
+++ LibSlyBroadcast/SlyBroadcast.cs
@@ -1,197 +1,200 @@
-﻿using System;
+﻿using LibSlyBroadcast.Extensions;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
-using LibSlyBroadcast.Extensions;
 
 namespace LibSlyBroadcast
 {
-	public enum WavOrMp3
-	{
-		Unset = 0,
-		Wav,
-		Mp3,
-	}
+    public enum WavOrMp3
+    {
+        Unset = 0,
+        Wav,
+        Mp3,
+    }
 
-	public class SlyBroadcast
-	{
-		// Required keys
-		static string keyUserId = "c_uid";
-		static string keyPassword = "c_password";
-		static string keyPhone = "c_phone";
-		static string keyRecordAudio = "c_record_audio";
-		static string keyCallerId = "c_callerID";
-		static string keyDeliveryDate = "c_date";
-		static string keyAudioFormat = "c_audio";
-		static string keyMobileOnly = "mobile_only";
+    public class SlyBroadcast
+    {
+        // Required keys
+        static string keyUserId = "c_uid";
+        static string keyPassword = "c_password";
+        static string keyPhone = "c_phone";
+        static string keyRecordAudio = "c_record_audio";
+        static string keyCallerId = "c_callerID";
+        static string keyDeliveryDate = "c_date";
+        static string keyAudioFormat = "c_audio";
+        static string keyMobileOnly = "mobile_only";
 
-		static string keyFileUrl = "c_url";
+        static string keyFileUrl = "c_url";
 
-		// Optional keys
-		static string keyPostbackUrl = "c_dispo_url";
-		static string keyEndDate = "c_endtime";
+        // Optional keys
+        static string keyPostbackUrl = "c_dispo_url";
+        static string keyEndDate = "c_endtime";
 
-		// Required values
-		public readonly string UserId;
-		public readonly string Password;
-		public readonly string[] PhoneNumbers;
-		public readonly string CallerId;
-		public readonly string DeliveryDate;
-		public readonly bool MobileOnly;
+        // Required values
+        public readonly string UserId;
+        public readonly string Password;
+        public readonly string[] PhoneNumbers;
+        public readonly string CallerId;
+        public readonly string DeliveryDate;
+        public readonly bool MobileOnly;
 
-		///<summary>Name of recording created via 'SlyBroadcast Recording Center' or uploaded to site.</summary>
-		public string RecordedAudioFileName;
+        ///<summary>Name of recording created via 'SlyBroadcast Recording Center' or uploaded to site.</summary>
+        public string RecordedAudioFileName;
 
-		// Required, but only if the other is also given
-		public string FileUrl;
-		public WavOrMp3 AudioFormat;
+        // Required, but only if the other is also given
+        public string FileUrl;
+        public WavOrMp3 AudioFormat;
 
-		// Optional values
-		public string PostbackUrl;
-		public string EndDate;
+        // Optional values
+        public string PostbackUrl;
+        public string EndDate;
 
-		bool setPreRecordedAudio = false;
-		bool setFileUrlAndAudioFormat = false;
+        bool setPreRecordedAudio = false;
+        bool setFileUrlAndAudioFormat = false;
 
-		bool IsValid => (AudioFormat != WavOrMp3.Unset && !string.IsNullOrWhiteSpace(FileUrl))
-			|| !string.IsNullOrWhiteSpace(RecordedAudioFileName);
+        bool IsValid => (AudioFormat != WavOrMp3.Unset && !string.IsNullOrWhiteSpace(FileUrl))
+            || !string.IsNullOrWhiteSpace(RecordedAudioFileName);
 
-		public SlyBroadcast(string userId, string password, string[] phoneNumbers, string callerId, string deliveryDate, bool mobileOnly)
-		{
-			if (string.IsNullOrWhiteSpace(userId))
-				throw new ArgumentNullException(nameof(userId));
+        public SlyBroadcast(string userId, string password, string[] phoneNumbers, string callerId, string deliveryDate, bool mobileOnly)
+        {
+            if (string.IsNullOrWhiteSpace(userId))
+                throw new ArgumentNullException(nameof(userId));
 
-			if (string.IsNullOrWhiteSpace(password))
-				throw new ArgumentNullException(nameof(password));
+            if (string.IsNullOrWhiteSpace(password))
+                throw new ArgumentNullException(nameof(password));
 
-			if (phoneNumbers == null || phoneNumbers.Length == 0)
-				throw new InvalidOperationException($"Must not provide an empty '{nameof(phoneNumbers)}' array to SlyBroadcast constructor.");
+            if (phoneNumbers == null || phoneNumbers.Length == 0)
+                throw new InvalidOperationException($"Must not provide an empty '{nameof(phoneNumbers)}' array to SlyBroadcast constructor.");
 
-			if (string.IsNullOrWhiteSpace(callerId))
-				throw new ArgumentNullException(nameof(callerId));
+            if (string.IsNullOrWhiteSpace(callerId))
+                throw new ArgumentNullException(nameof(callerId));
 
-			if (string.IsNullOrWhiteSpace(deliveryDate))
-				throw new ArgumentNullException(nameof(deliveryDate));
+            if (string.IsNullOrWhiteSpace(deliveryDate))
+                throw new ArgumentNullException(nameof(deliveryDate));
 
-			UserId = userId;
-			Password = password;
-			PhoneNumbers = phoneNumbers;
-			CallerId = callerId;
-			DeliveryDate = deliveryDate;
-			MobileOnly = mobileOnly;
+            UserId = userId;
+            Password = password;
+            PhoneNumbers = phoneNumbers;
+            CallerId = callerId;
+            DeliveryDate = deliveryDate;
+            MobileOnly = mobileOnly;
 
-			for (var i = 0; i < PhoneNumbers.Length; i++)
-				PhoneNumbers[i] = PhoneNumbers[i]
-					.Replace("-", "")
-					.Replace(" ", "")
-					.Replace("(", "")
-					.Replace(")", "")
-					.Replace(".", "");
-		}
+            for (var i = 0; i < PhoneNumbers.Length; i++)
+                PhoneNumbers[i] = PhoneNumbers[i]
+                    .Replace("-", "")
+                    .Replace(" ", "")
+                    .Replace("(", "")
+                    .Replace(")", "")
+                    .Replace(".", "");
+        }
 
-		public SlyBroadcast UsingPreRecordedAudio(string audioFileName)
-		{
-			if (setFileUrlAndAudioFormat)
-				throw new InvalidOperationException($"Must not call {nameof(UsingPreRecordedAudio)} after call to {nameof(UsingFileUrl)}");
+        public SlyBroadcast UsingPreRecordedAudio(string audioFileName)
+        {
+            if (setFileUrlAndAudioFormat)
+                throw new InvalidOperationException($"Must not call {nameof(UsingPreRecordedAudio)} after call to {nameof(UsingFileUrl)}");
 
-			if (setPreRecordedAudio)
-				throw new InvalidOperationException($"Must not call {nameof(UsingPreRecordedAudio)} multiple times.");
+            if (setPreRecordedAudio)
+                throw new InvalidOperationException($"Must not call {nameof(UsingPreRecordedAudio)} multiple times.");
 
-			if (string.IsNullOrWhiteSpace(audioFileName))
-				throw new ArgumentNullException(nameof(audioFileName));
+            if (string.IsNullOrWhiteSpace(audioFileName))
+                throw new ArgumentNullException(nameof(audioFileName));
 
-			setPreRecordedAudio = true;
-			RecordedAudioFileName = audioFileName;
-			return this;
-		}
+            setPreRecordedAudio = true;
+            RecordedAudioFileName = audioFileName;
+            return this;
+        }
 
-		public SlyBroadcast UsingFileUrl(string url, WavOrMp3 audioFormat)
-		{
-			if (setPreRecordedAudio)
-				throw new InvalidOperationException($"Must not call {nameof(UsingFileUrl)} after call to {nameof(UsingPreRecordedAudio)}");
+        public SlyBroadcast UsingFileUrl(string url, WavOrMp3 audioFormat)
+        {
+            if (setPreRecordedAudio)
+                throw new InvalidOperationException($"Must not call {nameof(UsingFileUrl)} after call to {nameof(UsingPreRecordedAudio)}");
 
-			if (setFileUrlAndAudioFormat)
-				throw new InvalidOperationException($"Must not call {nameof(UsingFileUrl)} multiple times.");
+            if (setFileUrlAndAudioFormat)
+                throw new InvalidOperationException($"Must not call {nameof(UsingFileUrl)} multiple times.");
 
-			if (audioFormat == WavOrMp3.Unset)
-				throw new ArgumentException($"Must not call {nameof(UsingFileUrl)} with {nameof(audioFormat)} of value {WavOrMp3.Unset}", nameof(audioFormat));
+            if (audioFormat == WavOrMp3.Unset)
+                throw new ArgumentException($"Must not call {nameof(UsingFileUrl)} with {nameof(audioFormat)} of value {WavOrMp3.Unset}", nameof(audioFormat));
 
-			if (string.IsNullOrWhiteSpace(url))
-				throw new ArgumentNullException(nameof(url));
+            if (string.IsNullOrWhiteSpace(url))
+                throw new ArgumentNullException(nameof(url));
 
-			setFileUrlAndAudioFormat = true;
-			FileUrl = url;
-			AudioFormat = audioFormat;
-			return this;
-		}
+            setFileUrlAndAudioFormat = true;
+            FileUrl = url;
+            AudioFormat = audioFormat;
+            return this;
+        }
 
-		/// <param name="postbackUrl">Raw URL, not urlencoded.</param>
-		public SlyBroadcast WithPostbackUrl(string postbackUrl)
-		{
-			if (string.IsNullOrWhiteSpace(postbackUrl))
-				throw new ArgumentNullException(nameof(postbackUrl));
+        /// <summary>Optional. Must be URL-Encoded if provided.</summary>
+        /// <param name="postbackUrl">The URL to POST data back to. Will be ignored if this is null, whitespace, or empty.</param>
+        public SlyBroadcast WithPostbackUrl(string postbackUrl)
+        {
+            if (string.IsNullOrWhiteSpace(postbackUrl))
+                return this;
 
-			PostbackUrl = postbackUrl;
-			return this;
-		}
+            PostbackUrl = postbackUrl;
+            return this;
+        }
 
-		public SlyBroadcast WithEndDate(string endDate)
-		{
-			if (string.IsNullOrWhiteSpace(endDate))
-				throw new ArgumentNullException(nameof(endDate));
+        /// <summary>Optional. Must be URL-Encoded if provided.</summary>
+        /// <param name="postbackUrl">The URL to POST data back to. Will be ignored if this is null, whitespace, or empty.</param>
+        public SlyBroadcast WithEndDate(string endDate)
+        {
+            if (string.IsNullOrWhiteSpace(endDate))
+                throw new ArgumentNullException(nameof(endDate));
 
-			EndDate = endDate;
-			return this;
-		}
+            EndDate = endDate;
+            return this;
+        }
 
-		Dictionary<string, string> ToParamDictionary()
-		{
-			if (!IsValid)
-				return null;
+        Dictionary<string, string> ToParamDictionary()
+        {
+            if (!IsValid)
+                return null;
 
-			var ret = new Dictionary<string, string>
-			{
-				[keyUserId] = UserId,
-				[keyPassword] = Password,
-				[keyPhone] = PhoneNumbers.JoinWith(","),
-				[keyCallerId] = CallerId,
-				[keyDeliveryDate] = DeliveryDate,
-				[keyMobileOnly] = MobileOnly ? "1" : "0",
-			};
+            var ret = new Dictionary<string, string>
+            {
+                [keyUserId] = UserId,
+                [keyPassword] = Password,
+                [keyPhone] = PhoneNumbers.JoinWith(","),
+                [keyCallerId] = CallerId,
+                [keyDeliveryDate] = DeliveryDate,
+                [keyMobileOnly] = MobileOnly ? "1" : "0",
+            };
 
-			if (!string.IsNullOrWhiteSpace(PostbackUrl))
-				ret[keyPostbackUrl] = HttpUtility.UrlEncode(PostbackUrl);
+            if (!string.IsNullOrWhiteSpace(PostbackUrl))
+                ret[keyPostbackUrl] = HttpUtility.UrlEncode(PostbackUrl);
 
-			if (!string.IsNullOrWhiteSpace(keyEndDate))
-				ret[keyEndDate] = EndDate;
+            if (!string.IsNullOrWhiteSpace(keyEndDate))
+                ret[keyEndDate] = EndDate;
 
-			if (AudioFormat == WavOrMp3.Unset)
-				ret[keyRecordAudio] = RecordedAudioFileName;
-			else
-			{
-				ret[keyFileUrl] = FileUrl;
-				ret[keyAudioFormat] = AudioFormat == WavOrMp3.Mp3 ? "mp3" : "wav";
-			}
+            if (AudioFormat == WavOrMp3.Unset)
+                ret[keyRecordAudio] = RecordedAudioFileName;
+            else
+            {
+                ret[keyFileUrl] = FileUrl;
+                ret[keyAudioFormat] = AudioFormat == WavOrMp3.Mp3 ? "mp3" : "wav";
+            }
 
-			return ret;
-		}
+            return ret;
+        }
 
-        /// <returns>The final, formatted URL that a GET request is sent to</returns>
+        /// <returns>LF (0a)-delimited strings, first will be either OK or ERROR depending on the status of the request.</returns>
 		public string SendMessage()
-		{
-			if (!IsValid)
-				return null;
+        {
+            if (!IsValid)
+                return null;
 
-			var url = "https://www.mobile-sphere.com/gateway/vmb.php?" +
-				ToParamDictionary().Select(kv => kv.Key + "=" + kv.Value)
-				.JoinWith("&");
+            var url = "https://www.mobile-sphere.com/gateway/vmb.php?" +
+                ToParamDictionary().Select(kv => kv.Key + "=" + kv.Value)
+                .JoinWith("&");
 
-			var req = WebRequest.Create(url);
-			using (var _ = req.GetResponse().GetResponseStream())
-			{ }
-
-			return url;
-		}
-	}
+            var req = WebRequest.Create(url);
+            using (var resp = req.GetResponse().GetResponseStream())
+            using (var s = new StreamReader(resp))
+                return s.ReadToEnd();
+        }
+    }
 }
